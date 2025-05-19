e91feab36bc5d24f4cf7f0a73970510940a1a1df
Mahmoud Niazy Elsolia <116367418+Mahmoud-Niazy@users.noreply.github.com>
2025-05-19 23:09:56 +0300

    blocked users manager

---
+++ lib/features/blocked_users/presentation/manager/blocked_users_cubit/blocked_users_cubit.dart
@@ -0,0 +1,50 @@
+import 'package:chat/core/failure/failure.dart';
+import 'package:chat/features/blocked_users/data/models/blocked_user_model/blocked_user_model.dart';
+import 'package:chat/features/blocked_users/domain/usecases/get_blocked_users_use_case.dart';
+import 'package:chat/features/blocked_users/domain/usecases/unblock_user_use_case.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'blocked_users_states.dart';
+
+class BlockedUsers extends Cubit<BlockedUsersStates> {
+  final GetBlockedUsersUseCase getBlockedUsersUseCase;
+  final UnblockUserUseCase unblockUserUseCase;
+
+  BlockedUsers(this.getBlockedUsersUseCase,
+      this.unblockUserUseCase) : super(BlockedUsersInitialState());
+
+  List<BlockedUserModel> blockedUsers = [];
+
+  Future<void> getBlockedUsers() async {
+    emit(GetBlockedUsersSuccessState());
+    try{
+      blockedUsers = await getBlockedUsersUseCase.execute();
+      emit(GetBlockedUsersSuccessState());
+    }
+    catch(error){
+      if(error is DioException){
+        emit(BlockedUsersErrorState(ServerFailure.fromDioException(error).error));
+      }
+      else{
+        emit(BlockedUsersErrorState(error.toString()));
+      }
+    }
+  }
+
+  Future<void> unblockUser(String userId)async{
+    emit(UnblockUserLoadingState());
+    try{
+      await unblockUserUseCase.execute(userId);
+      emit(UnblockUserSuccessState());
+    }
+    catch(error){
+      if(error is DioException){
+        emit(BlockedUsersErrorState(ServerFailure.fromDioException(error).error));
+      }
+      else{
+        emit(BlockedUsersErrorState(error.toString()));
+      }
+    }
+  }
+}
--- lib/features/blocked_users/presentation/manager/blocked_users_cubit/blocked_users_states.dart
+++ lib/features/blocked_users/presentation/manager/blocked_users_cubit/blocked_users_states.dart
@@ -2,7 +2,10 @@ abstract class BlockedUsersStates {}
 
 class BlockedUsersInitialState extends BlockedUsersStates{}
 
-class BlockedUsersErrorState extends BlockedUsersStates{}
+class BlockedUsersErrorState extends BlockedUsersStates{
+  final String error;
+  BlockedUsersErrorState(this.error);
+}
 
 class GetBlockedUsersLoadingState extends BlockedUsersStates{}
 class GetBlockedUsersSuccessState extends BlockedUsersStates{}
